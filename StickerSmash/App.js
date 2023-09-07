8149e57a2220a866a168eac5269d422a0b2c5954
guilherme2506 <guilherme.augd@gmail.com>
2023-09-07 15:08:34 -0300

    implemented the useState hook

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,4 +1,5 @@
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, View } from 'react-native';
 
@@ -9,6 +10,9 @@ import ImageViewer from './components/ImageViewer';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+const [selectedImage, setSelectedImage] = useState(null);
+
+
 const pickImageAsync = async () => {
   let result = await ImagePicker.launchImageLibraryAsync({
     allowsEditing: true,
@@ -16,7 +20,7 @@ const pickImageAsync = async () => {
   });
 
   if(!result.canceled) {
-    console.log(result);
+    setSelectedImage(result.assets[0].uri);
   }else {
     alert('You did not select any image.');
   }
@@ -26,7 +30,10 @@ const pickImageAsync = async () => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-      <ImageViewer placeholderImageSource={PlaceholderImage} />
+      <ImageViewer 
+        placeholderImageSource={PlaceholderImage} 
+        selectedImage={selectedImage}
+      />
       </View>
       <View style={styles.footerContainer}>
         <Button  theme= "primary" label="Choose a photo"  onPress={pickImageAsync}/>
