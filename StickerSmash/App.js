b82bd2ee9416646aef12896e3bea42d1f9ddbce7
guilherme2506 <guilherme.augd@gmail.com>
2023-09-09 17:21:07 -0300

    update showAppOptions rendering

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -53,11 +53,11 @@ const pickImageAsync = async () => {
       {showAppOptions ? (
         <View />
       ) : (
-      <View style={styles.footerContainer}>
-        <Button  theme= "primary" label="Choose a photo"  onPress={pickImageAsync}/>
-        <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
+        <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
       </View>
-      )}
+    )}
       <StatusBar style="auto" />
     </View>
   );
