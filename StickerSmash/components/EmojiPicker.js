b478d50793847194f1a808befee9c42269e856be
guilherme2506 <guilherme.augd@gmail.com>
2023-09-10 17:48:32 -0300

    added styling

--- StickerSmash/components/EmojiPicker.js
+++ StickerSmash/components/EmojiPicker.js
@@ -5,11 +5,39 @@ export default function EmojiPicker({isVisible, children, onClose}) {
     return (
         <Modal animationType = "slide" transparent={true} visible={isVisible}>
             <View>
-                <View>
-                    
+                <View style={styles.modalContent}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>Choose a sticker</Text>
+                    <Pressable onPress={onClose}>
+                        <MaterialIcons name="close" color="#fff" size={22} />
+                    </Pressable>
+                    </View>
+                    {children}
                 </View>
             </View>
 
         </Modal>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    modalContent: {
+        height: '25%',
+        width: '100%',
+        backgroundColor:'#25292e',
+        borderTopRightRadius: 18,
+        borderBottomLeftRadius: 18,
+        position: 'absolute',
+        bottom: 0,
+    },
+    titleContainer: {
+        height: '16%',
+        backgroundColor: '#464C55',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }
+})
