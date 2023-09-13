06dbda208b02863a0f8766f831e675212777e245
guilherme2506 <guilherme.augd@gmail.com>
2023-09-13 17:47:37 -0300

    fix the return block

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,86 +1,78 @@
-import * as ImagePicker from 'expo-image-picker';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View } from 'react-native';
 
-
-import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
-import CircleButton from './components/CircleButton';
-import IconButton from './components/IconButton';
-import EmojiPicker from './components/EmojiPicker';
+import Button from './components/Button';
 
-const PlaceholderImage = require('./assets/images/background-image.png');
+const PlaceholderImage = require('./assets/images/background-Image.png');
 
 export default function App() {
-const [isModalVisible, setIsModalVisible] = useState(false);
-const [selectedImage, setSelectedImage] = useState(null);
-const [showAppOptions, setShowAppOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
-const onReset = () => {
-  setShowAppOptions(false);
-};
+  
+  const pickImageAsync = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1, 
+    });
 
-const onAddSticker = () => {
-  setIsModalVisible(true);
-}
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    }else {
+      alert('You did not select any image.');
+    }
+  };
 
-const onModalClose = ()
-
-const onSaveImageAsync = async () => {
-  // we will implement this later
-}
-
-
-const pickImageAsync = async () => {
-  let result = await ImagePicker.launchImageLibraryAsync({
-    allowsEditing: true,
-    quality:1,
-  });
-
-  if(!result.canceled) {
-    setSelectedImage(result.assets[0].uri);
-    setShowAppOptions(true);
-  }else {
-    alert('You did not select any image.');
-  }
-};
-
+    // ...rest of the code remains same
+    const onReset = () => {
+      setShowAppOptions(false);
+    };
+  
+    const onAddSticker = () => {
+      // we will implement this later
+    };
+  
+    const onSaveImageAsync = async () => {
+      // we will implement this later
+    };
+  
 
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-      <ImageViewer 
+      <View style = {styles.imageContainer}>
+        <ImageViewer 
         placeholderImageSource={PlaceholderImage} 
-        selectedImage={selectedImage}  
-        
-      />
-     {showAppOptions ? (
-        <View style={styles.optionsContainer}>
-          <View style={styles.optionsRow}>
-            <IconButton icon="refresh" label="Reset" onPress={onReset} />
-            <CircleButton onPress={onAddSticker} />
-            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
-          </View>
+        selectedImage={selectedImage}
+        />
         </View>
-      ) : (
-        <View style={styles.footerContainer}>
+        {showAppOptions ? (
+         <View style={styles.optionsContainer}> 
+            <View style={styles.optionsRow}>
+              <IconButton icon="refresh" label="Reset" onPress={onReset} />
+              <CircleButton onPress={onAddSticker} />
+              <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+            </View>
+          </View>
+        ) : (
+      <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        <Button label="Use this photo" onPress={()=> setShowAppOptions(true)} />
       </View>
-    )}
+        )}
       <StatusBar style="auto" />
     </View>
   );
-
-  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   imageContainer: {
     flex: 1,
@@ -91,10 +83,16 @@ const styles = StyleSheet.create({
     height: 440,
     borderRadius: 18,
   },
-
   footerContainer: {
-    flex: 1/ 3,
+    flex: 1/3,
     alignItems: 'center',
   },
-});
-
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  }
+})
