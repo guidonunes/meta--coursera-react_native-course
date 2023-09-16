fd1a0e3041e5fa884d3fbc30d5d00aa7443dfe20
guilherme2506 <guilherme.augd@gmail.com>
2023-09-16 11:48:20 -0300

    added styling to the component

--- StickerSmash/components/EmojiList.js
+++ StickerSmash/components/EmojiList.js
@@ -31,3 +31,19 @@ export default function EmojiList({onSelect, onCloseModal}) {
         />
     );
 }
+
+const styles = StyleSheet.create({
+    listContainer: {
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginRight: 20,
+    },
+});
