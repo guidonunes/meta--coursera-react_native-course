c3d206767d138b6a42dfbf6889a927e0915fb536
guilherme2506 <guilherme.augd@gmail.com>
2023-09-30 17:12:16 -0300

    implemented useRef hook

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
@@ -61,8 +61,10 @@ export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
       </View>      
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
