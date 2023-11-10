e4ed35d490ac19b29ad65f355ae38e6def8f5b5e
guilherme2506 <guilherme.augd@gmail.com>
2023-11-10 11:26:25 -0300

    imported SubscribeScreen and WelcomeScreen components

--- newsletter-subscription-little-lemon/App.js
+++ newsletter-subscription-little-lemon/App.js
@@ -1,7 +1,11 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./navigators/RootNavigator";
-import { createNativeStackNavigator }
+import { createNativeStackNavigator } from  '@react-navigation/native-stack';
 
+import SubscribeScreen from './screens/SubscribeScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 // App.js is already setup by wrapping NavigationContainer around Root Navigator
 export default function App() {
   return (
