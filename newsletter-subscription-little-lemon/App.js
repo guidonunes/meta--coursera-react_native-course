281dd17b5c881b07dfd1e7f01e195ae74f80ddae
guilherme2506 <guilherme.augd@gmail.com>
2023-11-10 11:24:30 -0300

    installed stack navigator package

--- newsletter-subscription-little-lemon/App.js
+++ newsletter-subscription-little-lemon/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./navigators/RootNavigator";
+import { createNativeStackNavigator }
 
 // App.js is already setup by wrapping NavigationContainer around Root Navigator
 export default function App() {
