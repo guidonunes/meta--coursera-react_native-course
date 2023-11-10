2118000d463c2f579e73588c2613c825afd7ffb2
guilherme2506 <guilherme.augd@gmail.com>
2023-11-10 17:07:08 -0300

    updated import statements

--- newsletter-subscription-little-lemon/screens/WelcomeScreen.js
+++ newsletter-subscription-little-lemon/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
