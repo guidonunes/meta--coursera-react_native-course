6c73996841e6bec8cb62ebc35c5b01495aed5b83
guilherme2506 <guilherme.augd@gmail.com>
2023-11-18 16:11:10 -0300

    centered the logo image

--- newsletter-subscription-little-lemon/screens/WelcomeScreen.js
+++ newsletter-subscription-little-lemon/screens/WelcomeScreen.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
-import { View, Text, Image, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Image, Pressable, StyleSheet, Dimensions } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
-  // Add welcome screen code here.
+  
 
 const [newsletterButton, setNewsletterButton] = useState(false);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Little Lemon, your local Mediterranean Bistro</Text>
+      <Image style={styles.logo} source={require('../assets/little-lemon-logo.png')} />
+      <Text style={styles.welcomeText}>Little Lemon, your local Mediterranean Bistro!</Text>
    <Pressable
    style={styles.button}
    onPress={() => {
@@ -29,22 +30,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  logo: {
+    height: '30%',
+    width: '40%',
+    margin: 80,
+    justifyContent:'center',
+    resizeMode:'contain',
   },
   welcomeText: {
-    padding: 40,
-    fontSize: 30,
+    padding: 10,
+    fontSize: 20,
     color: '#333333',
     textAlign: 'center',
   }, 
   button: {
     backgroundColor: '#495E57',
     borderRadius: 12,
-    margin: 20,
+    margin: 80,
     padding: 15,
+    width: Dimensions.get('window').width * 0.8,
   },
   buttonText: {
     color: '#EDEFEE',
     textAlign: 'center',
-    fontSize: 22,
-  }
+    fontSize: 20,
+  },
+
 })
